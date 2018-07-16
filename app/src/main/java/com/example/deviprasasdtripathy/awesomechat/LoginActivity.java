5d1494279dd96d93e400dd3bb5999d8431857222
Devi Prasasd Tripathy <deviprasasdtripathy@Devis-MacBook-Air.local>
2018-07-16 04:03:50 -0500

    Google Sigin Added/Fixed

--- app/src/main/java/com/example/deviprasasdtripathy/awesomechat/LoginActivity.java
+++ app/src/main/java/com/example/deviprasasdtripathy/awesomechat/LoginActivity.java
@@ -116,6 +116,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         populateAutoComplete();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
